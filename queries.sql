70fb9c0a447053efb075637fc34e0f22b7f9cd5b
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-04-22 11:32:44 +0200

    Uitwerking van de exercise

--- queries.sql
+++ queries.sql
@@ -7,4 +7,4 @@ use netland;
 -- select title from series order by rating desc limit 1;
 -- select title from series where seasons <3 or seasons > 20;
 -- select * from series where title like 'th%';
-select title from series where seasons <3 or seasons >3;
+-- select title from series where seasons <3 or seasons >3;
