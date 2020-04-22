65a3c153013cb9044541f43fe8edbc65e6bbd1fa
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-04-22 11:31:51 +0200

    Uitwerking van de exercise

---
+++ queries.sql
@@ -0,0 +1,10 @@
+use netland;
+
+-- select title from series where has_won_awards = 1;
+-- select title from series where rating > 2.5;
+-- select title from series where language and country = 'NL';
+-- select title from series where seasons <5;
+-- select title from series order by rating desc limit 1;
+-- select title from series where seasons <3 or seasons > 20;
+-- select * from series where title like 'th%';
+select title from series where seasons <3 or seasons >3;
